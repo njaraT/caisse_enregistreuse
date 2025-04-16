564c5b72d7d5c68563f81dbace8454487d9f702b
njaraT <njaratianarandriambola4@gamil.com>
2025-04-16 13:56:40 +0400

    version 0.3

--- script/caisse.c
+++ script/caisse.c
@@ -1,36 +1,27 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include <stdbool.h>
 
-void rendre_monnaie(double montant) 
-{
-    int coupures[] = {2000, 1000, 500, 200, 100, 50, 25, 20, 10, 5, 1, 50, 20, 5};
-    int n = sizeof(coupures)/sizeof(coupures[0]);
+int stocks[] = {10,10,10,10,10,10,10,10,10,10,10,10,10,10};
+
+bool rendre_avec_stock(double montant) {
+    int coupures[] = {2000,1000,500,200,100,50,25,20,10,5,1,50,20,5};
     
-    for(int i = 0; i < n; i++) 
-    {
-        if(montant <= 0) break;
-        int valeur = coupures[i];
-        if(montant >= valeur) {
-            int nb = montant / valeur;
-            printf("%d x Rs%d\n", nb, valeur);
-            montant -= nb * valeur;
+    for(int i = 0; i < 14; i++) {
+        while(montant >= coupures[i] && stocks[i] > 0) {
+            printf("1 x Rs%d\n", coupures[i]);
+            montant -= coupures[i];
+            stocks[i]--;
         }
     }
+    return montant < 0.01;
 }
 
-int main() 
-{
-    srand(time(NULL));
-    double a_payer = (rand() % 10000) / 100.0;
-    double donne;
+int main() {
+    double a_payer = 115.50;
+    double donne = 200;
     
-    printf("À payer: Rs%.2f\n", a_payer);
-    do {
-        printf("Montant donné: Rs");
-        scanf("%lf", &donne);
-    } while(donne < a_payer);
-    
-    rendre_monnaie(donne - a_payer);
+    if(!rendre_avec_stock(donne - a_payer)) {
+        printf("Stock insuffisant!\n");
+    }
     return 0;
 }
